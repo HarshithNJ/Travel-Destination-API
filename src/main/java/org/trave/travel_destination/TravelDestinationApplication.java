d6473bdf8a4ecd96e7ee381a95d251e381ad1741
Harshith N J <njharshith122@gmail.com>
2025-01-26 12:20:19 +0530

    Started the Project

---
+++ src/main/java/org/trave/travel_destination/TravelDestinationApplication.java
@@ -0,0 +1,13 @@
+package org.trave.travel_destination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelDestinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelDestinationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/trave/travel_destination/TravelDestinationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.trave.travel_destination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelDestinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
